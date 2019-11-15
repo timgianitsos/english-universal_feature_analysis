0b63dbea2a48dcd17fb2ab20ce8d133ceb7e0c9a
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2019-11-14 22:57:53 -0800

    Prosody labels

---
+++ single-use-scripts/generate-mod-eng-labels.py
@@ -0,0 +1,24 @@
+'''
+Generate the csv of labels for files
+'''
+import os
+from os.path import join, dirname
+
+def main():
+	verse_dir = ('english-diachronic-corpus', 'Modern_English', 'Verse_Corpus')
+	prose_dir = ('english-diachronic-corpus', 'Modern_English', 'Plaintext_prose_corpus')
+	with open(join(dirname(__file__), '..', 'labels', 'modern_english_prosody.csv'), mode='w') as f:
+		f.write('verse:0,prose:1\n')
+		f.write('Filename,Label\n')
+		corpus = join(dirname(__file__), '..', *verse_dir)
+		for filename in os.listdir(corpus):
+			if filename.endswith('txt'):
+				f.write(f'{"/".join(verse_dir)}/{filename},0\n')
+		corpus = join(dirname(__file__), '..', *prose_dir)
+		for filename in os.listdir(corpus):
+			if filename.endswith('txt'):
+				f.write(f'{"/".join(prose_dir)}/{filename},1\n')
+	print('Success!')
+
+if __name__ == '__main__':
+	main()
